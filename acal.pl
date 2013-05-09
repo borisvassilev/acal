59804caf912350e516ef16c7a5a6ad7de4624478
Boris Vassilev <boris.vassilev@gmail.com>
2013-05-09 05:04:58 +0300

    Tokenize and parse input

--- acal.pl
+++ acal.pl
@@ -11,15 +11,56 @@ init :-
 cleanup :-
     write(bye), nl.
 
-quit?(end_of_file).
+quit(end_of_file).
 
 loop :-
     read_line_to_codes(user_input, Codes),
-    (  quit?(Codes)
+    (  quit(Codes)
     -> true
     ;  act(Codes), loop
     ).
 
 act(Codes) :-
-    format('~s~n', [Codes]).
+    tokenize(Codes, Tokens),
+    maplist(parse, Tokens, Parsed),
+    format('~w~n', [Parsed]).
 
+tokenize(Codes, Tokens) :-
+    phrase( line(Tokens), Codes ).
+
+parse(Token, Parsed) :-
+    phrase( parsed(Parsed), Token ).
+parse(Token, u(Unknown)) :-
+    atom_codes(Unknown, Token).
+
+:- use_module(library('dcg/basics')).
+
+line(Tokens) --> tokens(Tokens).
+tokens([]) --> blanks_to_nl, !.
+tokens([T|Ts]) --> token(T), tokens(Ts).
+
+token(T) --> whites, nonblanks(T).
+
+parsed(v(V)) --> number(V), !.
+parsed(a(AO)) --> arithop(AO), !.
+parsed(s(SO)) --> stackop(SO), !.
+parsed(l(LO)) --> listop(LO), !.
+parsed(c(C)) --> command(C), !.
+
+arithop(+) --> "+".
+arithop(-) --> "-".
+arithop(*) --> "*".
+arithop(/) --> "/".
+
+stackop(top) --> "top".
+stackop(show) --> "show".
+stackop(swap) --> "swap".
+stackop(duplicate) --> "duplicate".
+stackop(pop) --> "pop".
+stackop(revstack) --> "revstack".
+stackop(clear) --> "clear".
+
+listop(len) --> "len".
+listop(sum) --> "sum".
+
+command(quit) --> "quit".
